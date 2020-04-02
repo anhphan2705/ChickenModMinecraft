19796486c8bcaf97d9322e3475206b5fdde925fa
Anh Phan <59708955+anhphan2705@users.noreply.github.com>
2020-04-02 18:30:29 -0400

    Update Textures branch (#32)
    * Finish Chicken Fruit (#25) (#27)
    
    - Add effects (Night vision, speed 1, jump boost 2, luck) for 1:30 minutes
    - Reposition definition in file .lang
    
    * Update some attempts to make add loot item
    
    - Change loot_table
    - Fix RecourceLocation name error in Main
    - Add @EventBusSubsciber in LootEventHandler
    
    * Completely solve the droping problem
    
    * Update the probabilities for the pearl drop
    
    - 0.1 chance of dropping the pearl
    - double the chance if have lucky effect
    - might double the drop quantity if have loot enchantment

--- src/main/java/com/chickenmod/util/handlers/LootEventHandler.java
+++ src/main/java/com/chickenmod/util/handlers/LootEventHandler.java
@@ -8,24 +8,27 @@ import com.chickenmod.util.loot.LootUtils.ICondition;
 import com.chickenmod.util.loot.LootUtils.IMethod;
 
 import net.minecraft.world.storage.loot.LootTable;
+import net.minecraft.world.storage.loot.LootTableList;
 import net.minecraft.world.storage.loot.RandomValueRange;
 import net.minecraft.world.storage.loot.conditions.KilledByPlayer;
 import net.minecraft.world.storage.loot.conditions.LootCondition;
 import net.minecraft.world.storage.loot.functions.LootFunction;
 import net.minecraft.world.storage.loot.functions.LootingEnchantBonus;
 import net.minecraftforge.event.LootTableLoadEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+@EventBusSubscriber
 public class LootEventHandler {
 	
 	@SubscribeEvent
-	public void lootTableLoad(LootTableLoadEvent event) {
+	public static void lootTableLoad(LootTableLoadEvent event) {
 		
-
 		LootTable loot = event.getTable();
 		
-		if(event.getName().getResourcePath().equals("entities/chicken")) {
-			LootUtils.addItemToTable(loot, ModItems.CHICKEN_PEARL, 1, 1, 1, 1, 1, 1, 2, "cm:chicken_pearl",
+		if(LootTableList.ENTITIES_CHICKEN.equals(event.getName())) {
+			
+			LootUtils.addItemToTable(loot, ModItems.CHICKEN_PEARL, 1, 2F, 0.1F, 1, 1, 0F, 1F, "cm:chicken_pearl",
 			new IMethod() {
 				@Override
 				public void FunctionsCallback(ArrayList<LootFunction> lootfuncs) {
@@ -36,9 +39,24 @@ public class LootEventHandler {
 			new ICondition() {
 				@Override
 				public void FunctionsCallback(ArrayList<LootCondition> lootconds) {
-					lootconds.add(new KilledByPlayer(true));
+					lootconds.add(new KilledByPlayer(!true));
 				}
-				});
+			});
 		}
 	}
+	
+	//Second way of making a drop item
+	/*
+	if (LootTableList.ENTITIES_CHICKEN.equals(event.getName()))
+	{ 
+		event.getTable().getPool("main").addEntry(new LootEntryItem(
+			<reference to your item>,
+			<weight>,
+			<quality>,
+			<Lootfunction array>,
+			<Lootcondition array>,
+			<name for the entry>)); 
+	}
+	*/
+	
 }
